c87f60471dd5cab6d299b9e80ff264091ea6210c
Brad <44570552+br4dw@users.noreply.github.com>
2022-08-19 14:16:40 +0100

    fix: minor things changed

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -1,36 +1,38 @@
 <script>
-  export let content;
-  export let gradient;
-  const handleRedir = () => {
-    location.href = "https://github.com/brys0/Lightable/releases";
-  };
+    export let content
+    export let gradient
+    export let redirect
+    const handleRedir = () => {
+        location.href = redirect
+    };
 </script>
 
-<!-- <div class="button-container"> -->
-<button class="btn ns" style="background-image: linear-gradient({gradient.direction}, {gradient.colors.join(', ')});" aria-pressed="false" on:click={() => handleRedir()}>
-  <!-- <div class="icon"> -->
-  <slot />
-  <!-- </div> -->
-  {content}
+<button class="btn ns" style="background-image: linear-gradient({gradient?.direction}, {gradient?.colors.join(', ')});" on:click={handleRedir}>
+    <slot />
+    {content || 'Button'}
 </button>
 
-<!-- </div> -->
 <style lang="scss" scoped>
-  .btn {
-    all: unset;
-    padding: 1em;
-    border-radius: 0.5em;
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    align-items: center;
-    gap: 0.5em;
-    cursor: pointer;
-    transition: all 0.25s ease-in-out;
-    background-size: 100% 100%;
-    &:hover {
-      background-size: 200% 200%;
-      background-position-x: 100%;
+    .btn {
+        all: unset;
+        padding: 1em;
+        border-radius: 0.5em;
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        align-items: center;
+        gap: 0.5em;
+        cursor: pointer;
+        transition: all 0.25s ease-in-out;
+        background-size: 100% 100%;
+        &:hover {
+            background-size: 200% 200%;
+            background-position-x: 100%;
+        }
     }
-  }
+    @media screen and (max-width: 478px) {
+        .btn {
+            width: 125%;
+        } 
+    }
 </style>
--- src/components/Features.svelte
+++ src/components/Features.svelte
@@ -1,6 +1,6 @@
 <div class="features">
     <div class="feature">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><defs><style>.cls-1{opacity:0.3;}</style></defs><g id="Abstract_Elements" data-name="Abstract Elements"><g id="abs001-049"><g id="abs021-030"><g id="abs023"><path class="cls-1" d="M12,20.39a1,1,0,0,0,1.79.47l5.36-9.37A1,1,0,0,0,18.28,10H12Z"/><path d="M12,3.61a1,1,0,0,0-1.79-.47L4.85,12.51A1,1,0,0,0,5.72,14H12Z"/></g></g></g></g></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><defs><style>.cls-1{opacity:0.1;}</style></defs><path fill="#F36183" class="cls-1" d="M12,20.39a1,1,0,0,0,1.79.47l5.36-9.37A1,1,0,0,0,18.28,10H12Z"/><path fill="#F36183" d="M12,3.61a1,1,0,0,0-1.79-.47L4.85,12.51A1,1,0,0,0,5.72,14H12Z"/></svg>
         <div class="title ns">
             <p>Open Source</p>
         </div>
@@ -11,7 +11,7 @@
         </div>
     </div>
     <div class="feature">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><defs><style>.cls-1{opacity:0.3;}</style></defs><g id="Files_and_folders" data-name="Files and folders"><g id="fil011-020"><g id="fil016"><path class="cls-1" d="M10,4H21a1,1,0,0,1,1,1V7a0,0,0,0,1,0,0H10a0,0,0,0,1,0,0V4A0,0,0,0,1,10,4Z"/><path class="cls-1" d="M10.29,15.29l.71-.7-2.29-2.3a1,1,0,1,0-1.42,1.42l3,3A1,1,0,0,1,10.29,15.29Z"/><path d="M10.43,3.65,12,6h9a1,1,0,0,1,1,1V19a1,1,0,0,1-1,1H3a1,1,0,0,1-1-1V4A1,1,0,0,1,3,3H9.22A1.44,1.44,0,0,1,10.43,3.65Zm1.28,13.06,5-5a1,1,0,0,0-1.42-1.42L11,14.59l-2.29-2.3a1,1,0,1,0-1.42,1.42l3,3a1,1,0,0,0,1.42,0Z"/></g></g></g></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><defs><style>.cls-1{opacity:0.1;}</style></defs><path fill="#7380F3" class="cls-1" d="M10,4H21a1,1,0,0,1,1,1V7a0,0,0,0,1,0,0H10a0,0,0,0,1,0,0V4A0,0,0,0,1,10,4Z"/><path fill="#7380F3" class="cls-1" d="M10.29,15.29l.71-.7-2.29-2.3a1,1,0,1,0-1.42,1.42l3,3A1,1,0,0,1,10.29,15.29Z"/><path fill="#7380F3" d="M10.43,3.65,12,6h9a1,1,0,0,1,1,1V19a1,1,0,0,1-1,1H3a1,1,0,0,1-1-1V4A1,1,0,0,1,3,3H9.22A1.44,1.44,0,0,1,10.43,3.65Zm1.28,13.06,5-5a1,1,0,0,0-1.42-1.42L11,14.59l-2.29-2.3a1,1,0,1,0-1.42,1.42l3,3a1,1,0,0,0,1.42,0Z"/></svg>
         <div class="title ns">
             <p>Free. Forever.</p>
         </div>
@@ -22,13 +22,13 @@
         </div>
     </div>
     <div class="feature">
-        <svg id="General" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><defs><style>.cls-1{opacity:0.3;}</style></defs><g id="gen011-020"><g id="gen016"><path d="M15.43,8.57l-4.68,6.57a1,1,0,0,0-.09,1l2.54,5.31a1,1,0,0,0,.88.56h0a1,1,0,0,0,.88-.63l6.93-18a1,1,0,0,0-.22-1Z"/><path class="cls-1" d="M20.67,2.06,2.63,9A1,1,0,0,0,2,9.88a1,1,0,0,0,.56.92l5.31,2.54a1,1,0,0,0,1-.09l6.57-4.68,6.28-6.28A1,1,0,0,0,20.67,2.06Z"/></g></g></svg>
+        <svg id="General" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><defs><style>.cls-1{opacity:0.8;}</style></defs><path fill="#5AD7E9" d="M15.43,8.57l-4.68,6.57a1,1,0,0,0-.09,1l2.54,5.31a1,1,0,0,0,.88.56h0a1,1,0,0,0,.88-.63l6.93-18a1,1,0,0,0-.22-1Z"/><path fill="#5AD7E9" class="cls-1" d="M20.67,2.06,2.63,9A1,1,0,0,0,2,9.88a1,1,0,0,0,.56.92l5.31,2.54a1,1,0,0,0,1-.09l6.57-4.68,6.28-6.28A1,1,0,0,0,20.67,2.06Z"/></svg>
         <div class="title ns">
             <p>Find new friends!</p>
         </div>
         <div class="description">
             <p>
-               Find lovely people, to meet, talk, and love ❤️
+                Find lovely people, to meet, talk, and love ❤️
             </p>
         </div>
     </div>
@@ -37,6 +37,7 @@
 
 <style lang="scss" scoped>
     .features {
+        margin: 10em 0 10em 0;
         display: flex;
 		flex-direction: row;
         align-items: center;
@@ -50,7 +51,7 @@
         min-height: 10em;
         max-height: 10em;
     }
-    path { fill: white;}
+    // path { fill: white;}
     .feature {
         display: flex;
         flex-direction: column;
@@ -58,8 +59,8 @@
         align-items: center;
         word-wrap: break-word;
         gap: 1em;
-        width: 20em;
-        min-width: 20em;
+        width: 22em;
+        min-width: 16em;
         height: 22em;
         background-color: rgba(33, 34, 41, 0.938);
         border-radius: 1.5em;
